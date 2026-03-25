5895d5eea07b07b4d2fdc4433167654c118e0527
Maxi <maximilian.mau@remondis.de>
2026-03-25 22:53:16 +0100

    Favicon Tab angepasst

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -904,7 +904,11 @@
 
 <svelte:head>
 	<title>{$WEBUI_NAME}</title>
-	<link crossorigin="anonymous" rel="icon" href="{WEBUI_BASE_URL}/static/favicon.png" />
+	<link
+		crossorigin="anonymous"
+		rel="icon"
+		href="{WEBUI_BASE_URL}/static/favicon.png?v=red-logo"
+	/>
 
 	<meta name="apple-mobile-web-app-title" content={$WEBUI_NAME} />
 	<meta name="description" content={$WEBUI_NAME} />
